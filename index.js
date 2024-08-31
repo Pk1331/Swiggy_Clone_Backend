6acc8b011ded2b195199fe50c489cdb88ac08057
PavanKumar1174 <andepavan37@gmail.com>
2024-08-31 16:25:45 +0530

    CORS changed

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const app=express()
 const cors = require('cors')
 dotenv.config()
 const PORT=process.env.PORT || 5000
-app.use(cors())
+app.use(cors({
+    origin: 'https://swiggy-clone-dashboard-mu.vercel.app',
+    methods: 'GET,POST,PUT,DELETE',
+    credentials: true
+  }));
 app.use(body_parser.json())
 app.use('/vendor',Vendor_Routes)
 app.use('/firm',Firm_Routes)
